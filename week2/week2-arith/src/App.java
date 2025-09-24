59676685b600ff2da087a51516f5044a2c7d915a
Kimcrystal1004 <kb10703sj@gmail.com>
2025-09-25 04:38:39 +0900

    feat(week2): add addition and subtraction

---
+++ week2/week2-arith/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 5;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        // 커밋으로 아래 두 줄 추가
+        // System.out.println("a * b = " + (a * b));
+        // System.out.println("a % b = " + (a % b));
+    }
+}
